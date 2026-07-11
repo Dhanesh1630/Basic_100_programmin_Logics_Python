e897fc2ab3afce38703005ca21c4df55daa5a633
--get <--get>
2026-07-11 13:30:49 +0530

    To Read a Message

---
+++ quotient_remainder.py
@@ -0,0 +1,5 @@
+divident = int (input('enter divident: '))
+divisor = int (input('enter divisor: '))
+def divison (divident,divisor):
+    print(f"Quotient : {divident / divisor} , Remainder: {divident % divisor}")
+divison(divident,divisor)    
