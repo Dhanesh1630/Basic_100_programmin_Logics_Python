fe78ec05a725af78f4b39aebedf9f0e63ad25336
Bandiboyana Dhanesh <--get>
2026-07-17 19:08:09 +0530

    Program to find Square of a number without predefined method or '*'

---
+++ square_of_N.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+def squre_of_N(num):
+    result = 0
+    for i in range(num):
+        result += num
+    print(f"The square of {num} is: {result}")
+squre_of_N(num)  
